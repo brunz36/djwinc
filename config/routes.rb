1d464d864f9834bf7c3859e27353091006dc7eb0
Bruno Ribeiro <brunz36@gmail.com>
2017-05-21 17:14:28 -0400

    Added User and Session Model & Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get    'login'  => :new
+    post   'login'  => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
+
   get 'pages/education'
-
   get 'pages/splash'
-
   get 'pages/landing'
 
   root 'pages#landing'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521170645) do
+ActiveRecord::Schema.define(version: 20170521181426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20170521170645) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.string   "name"
-    t.boolean  "in_stock", default: true
+    t.boolean  "in_stock",      default: true
   end
 
   create_table "line_items", force: :cascade do |t|
@@ -72,6 +72,13 @@ ActiveRecord::Schema.define(version: 20170521170645) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_photos_on_item_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_foreign_key "line_items", "carts"
