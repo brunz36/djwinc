a11ac4bfdae1bbcc9c83d5eaf3dfb59ef064a04f
Bruno Ribeiro <brunz36@gmail.com>
2017-05-24 17:25:03 -0400

    Change To User Controller

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,5 @@ Rails.application.routes.draw do
     resources :items
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   mount Shrine::DownloadEndpoint => "/attachments"
 end
