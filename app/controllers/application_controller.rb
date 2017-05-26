2e1dc998cce9b6e72b18d9442b33207117740538
Bruno Ribeiro <brunz36@gmail.com>
2017-05-25 20:25:07 -0400

    Added Shipping Email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,16 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def logged_in?
+    !!current_user
+  end
+  helper_method :logged_in?
+
   def ssl_configured?
     Rails.env.production?
   end
