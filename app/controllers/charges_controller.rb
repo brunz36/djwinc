8690e6e0d7bb3cae6d79955a947c7723fbe3d324
Bruno Ribeiro <brunz36@gmail.com>
2017-05-30 23:40:06 -0400

    Changed CHarge Description

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -16,7 +16,7 @@ class ChargesController < ApplicationController
     charge = Stripe::Charge.create(
       customer: customer.id,
       amount: @amount,
-      description: '???????????',
+      description: 'Dinos Jewelry',
       currency: 'usd'
     )
 
