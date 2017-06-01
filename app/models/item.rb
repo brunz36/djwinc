e9ef0a15ae7982d1451832e5639c5758f2f09f3c
Bruno Ribeiro <brunz36@gmail.com>
2017-06-01 09:53:33 -0400

    Changed price validation to be lower

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  validates :price, length: { in: 6...10 }, numericality: true
+  validates :price, length: { in: 2...10 }, numericality: true
   validates :description, length: { in: 5..255 }
   validates :name, :category, :metal, presence: true
 
